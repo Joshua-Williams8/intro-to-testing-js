3deac5acf7d69851aad83ac6293f92d732052492
Joshua-Williams8 <joshua.n.williams8@gmail.com>
2021-03-24 17:04:49 -0500

    testing TDD

--- code.js
+++ code.js
@@ -1,4 +1,16 @@
-// helloWorld function 
-function helloWorld() {
-    return "Hello, World!";
+// helloWorld function
+//function expressions DO NOT get hoisted to the top.
+const helloWorld = function() {
+//function helloWorld() {
+  return "Hello, World!"
+  //return console.log("Hello, World!"); returns undefined
+}
+
+function addTwo(num){
+  //return
+  return 3
+}
+
+function sayHello() {
+  return
 }
--- tests.js
+++ tests.js
@@ -1,15 +1,52 @@
 // Unit tests for the helloWorld function
+// describe takes in a STRING, as a DESCRIPTION
+// 2nd input is a function that houses each INDIVIDUAL TEST
 describe('helloWorld', function() {
+  //Each individual test is defined with IT
+  //It takes in a STRING to ...
+  //the 2nd part is a function
     it('should be a defined function', function() {
       expect(typeof helloWorld).toBe('function');
+      //expect function is a a call to the typeof, and the .tobe is what I expect the output to be.
+      //output as in what I expect to happen and return this.
     });
     it('should return a string when called', function() {
+      //expecting the type of helloWorld() function toBe returned is "string"
       expect(typeof helloWorld()).toBe("string");
     });
     it('should return the string "Hello, World!" when executed', function() {
       expect(helloWorld()).toBe("Hello, World!");
     });
+    //.not.toBe would not return undefined.
     it("should never return 'undefined' when called", function() {
       expect(helloWorld()).not.toBe(undefined);
     });
+});
+//My stuff below test
+//When writing code for test, try to get away with bare minimum, so you can make sure you get the
+//outputs you want, so continue to
+describe("addTwo", function() {
+  it("should be a defined function", function() {
+    expect(typeof addTwo).toBe("function");
+  });
+  it("number 1 input should return the number 3",function() {
+    expect(addTwo(1)).toBe(3);
+  });
+  it("-8 input should return -6", function() {
+    expect(addTwo(-8)).toBe(-6);
+  });
+});
+describe("sayHello", function() {
+  it("Should be a defined function", function(){
+    expect(typeof sayHello).toBe("function");
+  });
+  it("should return a string", function() {
+    expect(typeof sayHello("")).toBe("string");
+  });
+  it('should return the string "Hello Josh" when executed', function() {
+    expect(sayHello("Josh")).toBe("Hello Josh");
+  });
+  it("should never return 'undefined' when called", function() {
+    expect(sayHello("")).not.toBe(undefined);
+  });
 });
